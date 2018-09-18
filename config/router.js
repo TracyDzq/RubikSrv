624da09a870cd84005c3971be1ac4e20c40cb1e4
TracyDzq <475018651@qq.com>
2018-09-18 15:27:08 +0800

    create futur

---
+++ config/router.js
@@ -0,0 +1,16 @@
+const Router = require('koa-router');
+const UserCol = require('../app/controllers/user');
+const checkToken = require('../app/controllers/app');
+const App = require('../app/controllers/app');
+
+module.exports = function() {
+  var router = new Router({
+    prefix: '/api'
+  });
+  router.post('/user/register', UserCol.reg); //注册
+  router.post('/user/login', UserCol.login); //登录
+  router.get('/user/allUsers', checkToken, UserCol.getAllUsers);
+  router.post('/user/del', checkToken, UserCol.delUser);
+
+  return router;
+};
